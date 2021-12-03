f7e354a4c4fbe9721c9bde1931c868c48419913b
MuazAlhaidar <omunam10@gmail.com>
2021-12-03 09:23:30 -0500

    Update MarketingAnalysis.scala

--- MarketingAnalysis.scala
+++ MarketingAnalysis.scala
@@ -4,7 +4,7 @@ import org.apache.spark.sql.functions.trim;
 var marketingDF = spark.read.format("csv").option("header","true").option("inferSchema","true").option("delimiter",";").option("quote","").load("EdurekaSparkProjects/dataset_bank-full.csv");
 
 for (colname <- marketingDF.columns) {
-	marketingDF = marketingDF.withColumn(colname.replace("""""", "").trim(col(colname), """""")))
+	marketingDF = marketingDF.withColumn(colname.replace("""""", "").trim(col(colname), """"""))
 	marketingDF = marketingDF.drop(colname)
 }
 
