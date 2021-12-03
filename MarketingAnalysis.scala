a6ecefefe9ccc031377563666624c312ed9ff8c7
MuazAlhaidar <omunam10@gmail.com>
2021-12-03 09:21:53 -0500

    Add files via upload

---
+++ MarketingAnalysis.scala
@@ -0,0 +1,11 @@
+import org.apache.spark.sql.functions._;
+import org.apache.spark.sql.functions.trim;
+
+var marketingDF = spark.read.format("csv").option("header","true").option("inferSchema","true").option("delimiter",";").option("quote","").load("EdurekaSparkProjects/dataset_bank-full.csv");
+
+for (colname <- marketingDF.columns) {
+	marketingDF = marketingDF.withColumn(colname.replace("""""", "").trim(col(colname), """""")))
+	marketingDF = marketingDF.drop(colname)
+}
+
+marketingDF.show()
