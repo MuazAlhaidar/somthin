77359c66807eea6f99e384ca7c16c22a0198258a
MuazAlhaidar <omunam10@gmail.com>
2021-12-04 18:16:12 -0500

    Update MarketingAnalysis.scala

--- MarketingAnalysis.scala
+++ MarketingAnalysis.scala
@@ -7,6 +7,7 @@ import org.apache.spark.ml.linalg.Vectors;
 import org.apache.spark.ml.feature.VectorAssembler;
 import org.apache.spark.sql.{Column => col};
 import org.apache.spark.ml.regression.LinearRegression;
+import org.apache.spark.sql.types.{StringType,NumericType,StructType,StructField};
 
 var spark = SparkSession.builder.getOrCreate();
 
@@ -14,12 +15,34 @@ var spark = SparkSession.builder.getOrCreate();
 // you are not supposed to bring any modifications in data. Any change in data is to done programmatically only.
 
 // 1.1 RDD -> DF
-var dataRDD = sc.textFile("EdurekaSparkProjects/dataset_bank-full.csv");
-var splitRDD = dataRDD.map(input => {
-	var cleanedValues = input.replace(";"," ").replace("""""","");
-	var splitvalues = "\s".r.split(cleanedValues);
-	// var splitvalues = ";".r.split(input);
+var dataRDD = sc.textFile("EdurekaSparkProjects/dataset_bank-full.csv").map(input => {
+	var cleanedValues = input.replace(";"," ").replace("\"", "");
+	var splitvalues = " ".r.split(cleanedValues);
 	splitvalues;
 });
 
-dataRDD.take(10)
+var topRowRemovedRDD = dataRDD.zipWithIndex().collect {
+  case (v, index) if index != 0 => v;
+};
+
+var mySchema = StructType(Array(
+	StructField("age",NumericType,true),
+	StructField("job", StringType, true),
+	StructField("marital", StringType, true),
+	StructField("education", StringType, true),
+	StructField("default", StringType, true),
+	StructField("balance", NumericType, true),
+	StructField("housing", StringType, true),
+	StructField("loan", StringType, true),
+	StructField("contact", StringType, true),
+	StructField("day", NumericType, true),
+	StructField("month", StringType, true),
+	StructField("duration", NumericType, true),
+	StructField("campaign", NumericType, true),
+	StructField("pdays", NumericType, true),
+	StructField("previous", NumericType, true),
+	StructField("poutcome", StringType, true),
+	StructField("y", StringType, true),
+));
+
+var dataDF = spark.createDataFrame(topRowRemovedRDD, mySchema);
