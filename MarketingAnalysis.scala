12c848552084e82870898161bc5788aedf9c8758
MuazAlhaidar <omunam10@gmail.com>
2021-12-04 19:06:19 -0500

    Update MarketingAnalysis.scala

--- MarketingAnalysis.scala
+++ MarketingAnalysis.scala
@@ -1,3 +1,4 @@
+import org.apache.spark.sql._;
 import org.apache.spark.sql.functions._;
 import org.apache.spark.sql.functions.trim;
 import org.apache.spark.SparkConf;
@@ -7,9 +8,7 @@ import org.apache.spark.ml.linalg.Vectors;
 import org.apache.spark.ml.feature.VectorAssembler;
 import org.apache.spark.sql.{Column => col};
 import org.apache.spark.ml.regression.LinearRegression;
-import org.apache.spark.sql.types.{StringType,IntegerType,StructType,StructField};
-
-var spark = SparkSession.builder.getOrCreate();
+import org.apache.spark.sql.types.{StringType,DoubleType,StructType,StructField};
 
 // 1 Load data and create Spark dataframe, Spark RDD : 
 // you are not supposed to bring any modifications in data. Any change in data is to done programmatically only.
@@ -25,24 +24,10 @@ var topRowRemovedRDD = dataRDD.zipWithIndex().collect {
   case (v, index) if index != 0 => v;
 };
 
-var mySchema = StructType(Array(
-	StructField("age",IntegerType,true),
-	StructField("job", StringType, true),
-	StructField("marital", StringType, true),
-	StructField("education", StringType, true),
-	StructField("default", StringType, true),
-	StructField("balance", IntegerType, true),
-	StructField("housing", StringType, true),
-	StructField("loan", StringType, true),
-	StructField("contact", StringType, true),
-	StructField("day", IntegerType, true),
-	StructField("month", StringType, true),
-	StructField("duration", IntegerType, true),
-	StructField("campaign", IntegerType, true),
-	StructField("pdays", IntegerType, true),
-	StructField("previous", IntegerType, true),
-	StructField("poutcome", StringType, true),
-	StructField("y", StringType, true)
-));
+var rowRDD = topRowRemovedRDD.map(input => {
+	Row(input(0),input(1),input(2),input(3),input(4),input(5),input(6),input(7),input(8),input(9),input(10),input(11),input(12),input(13),input(14),input(15),input(16));
+});
 
-var dataDF = spark.createDataFrame(topRowRemovedRDD, mySchema);
+var mySchema = StructType(Array( StructField("age",DoubleType,true),StructField("job", StringType, true),StructField("marital", StringType, true),StructField("education", StringType, true),StructField("default", StringType, true),StructField("balance", DoubleType, true),StructField("housing", StringType, true),StructField("loan", StringType, true),StructField("contact", StringType, true),StructField("day", DoubleType, true),StructField("month", StringType, true),StructField("duration", DoubleType, true),StructField("campaign", DoubleType, true),StructField("pdays", DoubleType, true),StructField("previous", DoubleType, true),StructField("poutcome", StringType, true),StructField("y", StringType, true)));
+
+var dataDF = spark.createDataFrame(rowRDD, mySchema);
